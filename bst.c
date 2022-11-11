6d520a1ec145d4347855a472dde2df3d2213ded1
adityagothiwarekar <109978509+adityagothiwarekar@users.noreply.github.com>
2022-11-11 23:46:50 +0530

    Update bst.c

--- bst.c
+++ bst.c
@@ -1,61 +1,143 @@
-struct bst 
-{
-    int data;
-    struct bst *left;
-    struct bst *right;
-};
-
-struct bst* getnode()
-{
-    return (struct bst*) malloc(sizeof(struct bst));
+# include <stdio.h>
+#include <stdlib.h>
+struct bt {
+int info;
+struct bt *left,*right;
+}node;
+struct bt *root=NULL;
+static int counter=0;
+struct bt* getnode (){
+ counter++;
+ return (( struct bt* ) malloc (sizeof(struct bt)));
 }
-
-struct bst* insert(struct bst *q, int val)
-{
-    struct bst* temp = NULL;
-    if(q == NULL)
-    {
-        temp = getnode();
-        temp->data = val;
-        temp->left = NULL;
-        temp->right = NULL;
-        q = temp;
-    }
-    else
-    {
-        if(val < q->data) 
-        {
-            q->left = insert(q->left, val);
-        }
-        else
-        {
-            q->right = insert(q->right, val);
-        }
-    }
-    return q;
+void freenode(struct bt *p){
+ counter--;
+ free(p);
 }
-
-struct bst* search(struct bst *p, int key, struct bst **y)
+struct bt* insert(struct bt *r,int x){
+ struct bt *newnode;
+ if(x==r->info)
+ printf("\n Duplicate Values.");
+ else if(x<r->info)
+ {
+ if(r->left==NULL)
+ {
+ newnode=getnode();
+ newnode->info=x;
+ newnode->left=NULL;
+ newnode->right= NULL;
+ r->left=newnode;
+ }
+ else
+ insert(r->left,x);
+ }
+ else if(x>r->info)
+ {
+ if(r->right==NULL)
+ {
+ newnode=getnode();
+ newnode->info=x;
+ newnode->left=NULL;
+ newnode->right= NULL;
+ r->right=newnode;
+ }
+ else
+ insert(r->right,x);
+ }
+}
+void inorder(struct bt *r)
 {
-    struct bst *temp = NULL;
-    if(p == NULL)
-    {
-        return NULL;
-    }
-    temp = p;
-    *y = NULL;
-    while(temp!=NULL)
-    {
-        if(temp->data == key)
-            return (temp);
-        else 
-        {
-            *y = temp;
-            if(temp->data > key)
-                temp = temp->left;
-            else 
-                temp = temp->right;
-        }
-    }
-    return (NULL);
+ if(r!=NULL)
+ {
+ inorder(r->left);
+ printf("\n\t %d ",r->info);
+ inorder(r->right);
+ }
+}
+void preorder(struct bt *r)
+{
+ if(r!=NULL)
+ {
+ printf("\n\t %d ",r->info);
+ preorder(r->left);
+ preorder(r->right);
+ }
+} 
+void postorder(struct bt *r)
+{
+ if(r!=NULL)
+ {
+ postorder(r->left);
+ postorder(r->right);
+ printf("\n\t %d ",r->info);
+ }
+}
+int count()
+{ 
+ printf(" Number of nodes present : %d",counter); 
+}
+void search(struct bt *r)
+{
+ int found=0, x;
+ struct bt *temp;
+ temp=root;
+ printf("\nEnter the node to be searched : ");
+ scanf("%d", &x);
+ while(temp!=NULL)
+ {
+ if(temp->info==x)
+ {
+ printf("\nNode %d is present",temp->info);
+ found=1;
+ break;
+ }
+ if(temp->info>x)
+ temp=temp->left;
+ else
+ temp=temp->right;
+ }
+ if(found==0)
+ printf("\nElement not found");
+}
+int main ()
+{
+ int ch,x;
+ struct bt *newnode;
+ while(1)
+ {
+ printf("\n ----- Linked List -----");
+ printf("\n 1. Insert");
+ printf("\n 2. PreOrder");
+ printf("\n 3. InOrder");
+ printf("\n 4. PostOrder");
+ printf("\n 5. Search");
+ printf("\n 6. Count \n 7. Exit");
+ printf("\n Enter your Choice : ");
+ scanf("%d",&ch);
+ switch(ch)
+ {
+ case 1: printf(" Enter the node value:");
+ scanf("\n %d",&x);
+ if(root==NULL)
+ {
+ newnode=getnode();
+ newnode->info=x;
+ newnode->left=NULL;
+ newnode->right=NULL;
+ root=newnode;
+ }
+ else
+ insert(root,x);
+ break;
+ case 2 : preorder(root); break;
+ case 3 : inorder(root); break;
+ case 4 : postorder(root); break;
+ case 5 : search(root);
+ break;
+ case 6 : count(); break;
+ case 7 : return 0;
+ default : printf ("\n Wrong Choice");
+ 
+ }
+ }
 }
